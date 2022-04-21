79f6d2d74eaa1c6bbb0beeb7cfbe3b30b3ea7d5c
alexandriaC7 <alexandriachang7@gmail.com>
2022-04-21 12:31:16 +0800

    test
    Signed-off-by: alexandriaC7 <alexandriachang7@gmail.com>

--- .drone.sh
+++ .drone.sh
@@ -1,40 +1,44 @@
-#!/bin/bash
-#
-
-START=$(date +"%s")
-export KBUILD_BUILD_HOST=07
-export KBUILD_BUILD_USER="alexandria"
-TC_DIR="$HOME/tc/aosp-clang"
-GCC_64_DIR="$HOME/tc/aarch64-linux-android-4.9"
-GCC_32_DIR="$HOME/tc/arm-linux-androideabi-4.9"
-AK3_DIR="$HOME/android/AnyKernel3"
-DEFCONFIG="vendor/kona-perf_defconfig"
-VERSION="$(cat arch/arm64/configs/vendor/kona-perf_defconfig | grep "CONFIG_LOCALVERSION\=" | sed -r 's/.*"(.+)".*/\1/' | sed 's/^.//')"
-
+#!/usr/bin/env bash
 echo "Cloning dependencies"
-apt-get -y install sudo python3-pip curl unzip p7zip-full curl python2 binutils-aarch64-linux-gnu wget binutils-aarch64-linux-gnu binutils-arm-linux-gnueabi libtinfo5 -yq xxd python
+apt-get -y install sudo python3-pip curl unzip p7zip-full curl python2 binutils-aarch64-linux-gnu wget binutils-aarch64-linux-gnu binutils-arm-linux-gnueabi libtinfo5 -yq xxd python build-essential make
 sudo ln -sf /usr/share/zoneinfo/Asia/Singapore /etc/localtime
-git clone --depth=1 https://github.com/ArrowOS-Devices/android_prebuilts_clang_host_linux-x86_clang-r428724 $TC_DIR
-git clone https://github.com/sohamxda7/llvm-stable -b gcc64 --depth=1 $GCC_64_DIR
-git clone https://github.com/sohamxda7/llvm-stable -b gcc32  --depth=1 $GCC_32_DIR
+git clone --depth=1 https://github.com/alexandriaC7/kernel_oneplus_kebab kernel && cd kernel
+#git clone --depth=1 https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r450784 clang
+#git clone https://github.com/greenforce-project/gcc-arm64 --depth=1 gcc
+#git clone https://github.com/greenforce-project/gcc-arm32 --depth=1 gcc32
+git clone --depth=1 https://github.com/alexandriaC7/AnyKernel3 AnyKernel3
+git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
+git clone --depth=1 https://android.googlesource.com/platform/prebuilts/misc prebuilts/misc
 echo "Done"
 
-export PATH="$TC_DIR/bin:$PATH"
+DTC=$(pwd)/prebuilts/misc/linux-x86/dtc/dtc
+DTC_EXT=$(pwd)/prebuilts/misc/linux-x86/dtc/dtc
+IMAGE=$(pwd)/out/arch/arm64/boot/Image
+START=$(date +"%s")
+KERNEL_DIR=$(pwd)
+PATH="${PWD}/clang/bin:$PATH"
+export KBUILD_COMPILER_STRING="$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
+export ARCH=arm64
+export KBUILD_BUILD_HOST="laplace"
+export KBUILD_BUILD_USER="alexandria"
 
-mkdir -p out
-make O=out ARCH=arm64 $DEFCONFIG
+# Compile plox
+function compile() {
+    make O=out ARCH=arm64 vendor/kona-perf_defconfig
+    make -j$(nproc --all) O=out \
+                          ARCH=arm64 \
+			  CC=clang \
+			  CROSS_COMPILE=aarch64-linux-gnu- \
+			  CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 
-echo -e "\nStarting compilation...\n"
-make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=$GCC_64_DIR/bin/aarch64-linux-android- CROSS_COMPILE_ARM32=$GCC_32_DIR/bin/arm-linux-androideabi- CLANG_TRIPLE=aarch64-linux-gnu- Image.gz dtbo.img
+}
 
-kernel="out/arch/arm64/boot/Image"
-dtb="out/arch/arm64/boot/dts/qcom/kona.dtb"
-dtbo="out/arch/arm64/boot/dtbo.img"
+compile
 
 # Build flashable zip
 cp out/arch/arm64/boot/Image AnyKernel3/
 cp out/arch/arm64/boot/dtbo.img AnyKernel3/
-zipfile="./out/$VERSION-lemonkebab-$(date +%Y%m%d-%H%M).zip"
+zipfile="./out/kernel-lemonkebab-$(date +%Y%m%d-%H%M).zip"
 7z a -mm=Deflate -mfb=258 -mpass=15 -r $zipfile ./AnyKernel3/*
 
 END=$(date +"%s")
@@ -43,4 +47,5 @@ curl --upload-file $zipfile https://transfer.sh; echo
 sleep 2
 #sometimes transfer.sh couldnt save metadata 
 curl -F "file=@$zipfile" https://file.io
+
 
