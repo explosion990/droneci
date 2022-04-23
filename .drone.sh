0e28b5ddc6ed8415b8c3ae82b77ff35dc809a4ef
alexandriaC7 <alexandriachang7@gmail.com>
2022-04-23 11:14:47 +0800

    test

--- .drone.sh
+++ .drone.sh
@@ -1,35 +1,60 @@
 #!/usr/bin/env bash
 echo "Cloning dependencies"
-apt-get -y install sudo python3-pip curl unzip p7zip-full curl python2 binutils-aarch64-linux-gnu wget binutils-aarch64-linux-gnu binutils-arm-linux-gnueabi libtinfo5 -yq xxd python build-essential make
+apt-get -y install sudo python3-pip curl unzip p7zip-full curl python2 binutils-aarch64-linux-gnu wget binutils-aarch64-linux-gnu binutils-arm-linux-gnueabi libtinfo5 -yq xxd python build-essential make gcc-aarch64-linux-gnu
 sudo ln -sf /usr/share/zoneinfo/Asia/Singapore /etc/localtime
-git clone --depth=1 https://github.com/alexandriaC7/kernel_oneplus_kebab kernel && cd kernel
-#git clone --depth=1 https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r450784 clang
-#git clone https://github.com/greenforce-project/gcc-arm64 --depth=1 gcc
-#git clone https://github.com/greenforce-project/gcc-arm32 --depth=1 gcc32
-git clone --depth=1 https://github.com/alexandriaC7/AnyKernel3 AnyKernel3
-git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
+git clone --depth=1 https://github.com/hydrangea07/kernel_oneplus_kebab kernel && cd kernel
+git clone --depth=1 https://github.com/hydrangea07/AnyKernel3 AnyKernel3
+git clone --depth=1 https://github.com/pkm774/android-kernel-tools ktools
 git clone --depth=1 https://android.googlesource.com/platform/prebuilts/misc prebuilts/misc
 echo "Done"
-
+           
+           
 DTC=$(pwd)/prebuilts/misc/linux-x86/dtc/dtc
 DTC_EXT=$(pwd)/prebuilts/misc/linux-x86/dtc/dtc
-IMAGE=$(pwd)/out/arch/arm64/boot/Image
 START=$(date +"%s")
-KERNEL_DIR=$(pwd)
-PATH="${PWD}/clang/bin:$PATH"
-export KBUILD_COMPILER_STRING="$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
+PATH="$(pwd)/ktools/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:$PATH"
+PATH="$(pwd)/ktools/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin:$PATH"
+PATH="$(pwd)/ktools/clang/host/linux-x86/clang-r428724/bin:$PATH"
+export LD_LIBRARY_PATH="$(pwd)/ktools/clang/host/linux-x86/clang-r428724/lib64:$LD_LIBRARY_PATH"
 export ARCH=arm64
+export SUBARCH=ARM64
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export CROSS_COMPILE=aarch64-linux-android-
+export CROSS_COMPILE_ARM32=arm-linux-androideabi-
 export KBUILD_BUILD_HOST="laplace"
 export KBUILD_BUILD_USER="alexandria"
 
 # Compile plox
 function compile() {
-    make O=out ARCH=arm64 vendor/kona-perf_defconfig
-    make -j$(nproc --all) O=out \
-                          ARCH=arm64 \
-			  CC=clang \
-			  CROSS_COMPILE=aarch64-linux-gnu- \
-			  CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+make \
+	O=out \
+	clean \
+	mrproper \
+	CC=clang \
+	AR=llvm-ar \
+	NM=llvm-nm \
+	OBJCOPY=llvm-objcopy \
+	OBJDUMP=llvm-objdump \
+	READELF=llvm-readelf \
+	OBJSIZE=llvm-size \
+	STRIP=llvm-strip \
+	HOSTCC=clang \
+	HOSTCXX=clang++ \
+	vendor/kona-perf_defconfig
+  
+make \
+	O=out \
+	CC=clang \
+	AR=llvm-ar \
+	NM=llvm-nm \
+	OBJCOPY=llvm-objcopy \
+	OBJDUMP=llvm-objdump \
+	READELF=llvm-readelf \
+	OBJSIZE=llvm-size \
+	STRIP=llvm-strip \
+	HOSTCC=clang \
+	HOSTCXX=clang++ \
+	-j$(nproc --all)
 
 }
 
